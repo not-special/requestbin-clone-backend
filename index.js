b9f2526398bc072efa07d6c94dd1b5e526b12f87
Scott Graham <scttgrhm7+public@gmail.com>
2022-06-02 14:27:39 -0700

    Add cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const bodyParser = require('body-parser')
 require('dotenv').config()
 const middleware = require('./utils/middleware')
 const PORT = process.env.PORT
 
+app.use(cors())
 app.use(bodyParser.json())
 app.use(middleware.requestLogger)
 
