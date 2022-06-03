4458fd6082c0d40f566f73a027c7a60bf0026234
Scott Graham <scttgrhm7+public@gmail.com>
2022-06-02 18:28:00 -0700

    Add functioning mongo get and set

---
+++ models/request.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const url = process.env.MONGODB_URI
+
+console.log('connecting to', url)
+
+mongoose.connect(url)
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+  const requestSchema = new mongoose.Schema({
+    id: Number,
+    binId: Number,
+    payload: Object,
+  })
+
+module.exports = mongoose.model('Request', requestSchema)
